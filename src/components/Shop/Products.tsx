d5a5e68365be6f3366993b382973eac6afdffbb3
LordCracken <vladislav070201@gmail.com>
2022-11-14 23:02:02 +0300

    move products data to backend

--- src/components/Shop/Products.tsx
+++ src/components/Shop/Products.tsx
@@ -1,19 +1,27 @@
+import {useState, useEffect} from 'react';
+
 import ProductItem from './ProductItem';
 
 import { IProductItem } from '../../interfaces';
 import classes from './Products.module.css';
 
-const DUMMY_PRODUCTS: IProductItem[] = [
-  { id: 'p1', price: 6, title: 'My First Book', description: 'The first book I ever wrote' },
-  { id: 'p2', price: 5, title: 'My Second Book', description: 'The second book I ever wrote' },
-];
-
 const Products = () => {
+  const [products, setProducts] = useState<IProductItem[]>([]);
+
+  useEffect(() => {
+    (async ()=>{
+      const url = 'https://reduxcart-b32e8-default-rtdb.europe-west1.firebasedatabase.app/products.json';
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+    })();
+  }, []);
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        {DUMMY_PRODUCTS.map(product => (
+        {products.map(product => (
           <ProductItem
             key={product.id}
             id={product.id}
