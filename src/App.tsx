d65f089913340e3e019b08cc1583ad5927e59db2
LordCracken <vladislav070201@gmail.com>
2022-11-15 00:20:56 +0300

    send cart data to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -8,6 +9,21 @@ import { RootState } from './store';
 
 const App = () => {
   const showCart = useSelector((state: RootState) => state.ui.cartIsVisible);
+  const cart = useSelector((state: RootState) => state.cart);
+
+  useEffect(() => {
+    (async () => {
+      const url =
+        'https://reduxcart-b32e8-default-rtdb.europe-west1.firebasedatabase.app/cart.json';
+      const response = await fetch(url, { method: 'PUT', body: JSON.stringify(cart) });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      const data = await response.json();
+    })();
+  }, [cart]);
 
   return (
     <Layout>
--- src/components/Shop/Products.tsx
+++ src/components/Shop/Products.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 import ProductItem from './ProductItem';
 
@@ -9,9 +9,15 @@ const Products = () => {
   const [products, setProducts] = useState<IProductItem[]>([]);
 
   useEffect(() => {
-    (async ()=>{
-      const url = 'https://reduxcart-b32e8-default-rtdb.europe-west1.firebasedatabase.app/products.json';
+    (async () => {
+      const url =
+        'https://reduxcart-b32e8-default-rtdb.europe-west1.firebasedatabase.app/products.json';
       const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error('Could not get products list.');
+      }
+
       const data = await response.json();
       setProducts(data);
     })();
